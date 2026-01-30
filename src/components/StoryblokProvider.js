fc52ccb224ced5c51e12af97641228975cf7d85b
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2026-01-30 14:58:38 -0800

    Revert "set up visual editor detection and cache bypass. V2"
    This reverts commit 806a0466404cde2a59897029478ac754e3c60f36.

--- src/components/StoryblokProvider.js
+++ src/components/StoryblokProvider.js
@@ -1,22 +1,8 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import { disablePreviewAction } from "@/app/actions/disablePreview"
 import { getStoryblokApi } from "@/lib/storyblok"
-import { initStoryblokClient } from "@/lib/storyblok-client"
 
 export default function StoryblokProvider({ children }) {
-  const [isVisualEditor, setIsVisualEditor] = useState(false)
-
-  useEffect(() => {
-    if (window.self !== window.top && window.location.search.includes("_storyblok")) {
-      setIsVisualEditor(true)
-    } else {
-      disablePreviewAction()
-    }
-  }, [])
-
-  if (isVisualEditor) initStoryblokClient()
   getStoryblokApi()
   return children
 }
--- src/lib/getStoryBlokData.js
+++ src/lib/getStoryBlokData.js
@@ -2,15 +2,10 @@ import { getStoryblokApi } from "@/lib/storyblok"
 
 const getStoryBlokData = async (url, options = {}) => {
   const apiOptions = {
-    // version: process.env.NODE_ENV === "production" ? "published" : "draft",
-    version: "production",
+    version: process.env.NODE_ENV === "production" ? "published" : "draft",
     ...options
   }
   const { data } = await getStoryblokApi().get(url, apiOptions)
-
-  // const x = await getStoryblokApi()
-  // console.log("JPH x was", x)
-  // const { data } = x.get(url, apiOptions)
   return data
 }
 
