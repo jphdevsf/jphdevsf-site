766cc5137a7ec20f87b032d7665d778faec49e43
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2026-01-30 23:10:21 -0800

    force dynamic render on next.js side to support preview mode in production environment

--- src/app/[[...slug]]/page.js
+++ src/app/[[...slug]]/page.js
@@ -2,6 +2,10 @@ import { StoryblokStory } from "@storyblok/react/rsc"
 import { notFound } from "next/navigation"
 import getStoryBlokData from "@/lib/getStoryBlokData"
 
+// Forcing dynamic rendering here to support visual editor draft mode with a single production environment. Hurts performance on static render side, but we still have netlify edge cache.
+// Consider removing if we move to dedicated preview environment.
+export const dynamic = "force-dynamic"
+
 const Page = async ({ params }) => {
   const { slug } = await params
   const fullSlug = slug ? slug.join("/") : "home"
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,10 @@
 import { StoryblokStory } from "@storyblok/react/rsc"
 import getStoryBlokData from "@/lib/getStoryBlokData"
 
+// Forcing dynamic rendering here to support visual editor draft mode with a single production environment. Hurts performance on static render side, but we still have netlify edge cache.
+// Consider removing if we move to dedicated preview environment.
+export const dynamic = "force-dynamic"
+
 const NotFound = async () => {
   try {
     const data = await getStoryBlokData("cdn/stories/not-found")
